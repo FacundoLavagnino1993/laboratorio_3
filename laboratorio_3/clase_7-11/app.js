7ae1ba10a6505e376389bcd49da1728312c9bd53
alumno <alumno>
2017-11-07 21:03:44 -0300

    clase 7-11

---
+++ laboratorio_3/clase_7-11/app.js
@@ -0,0 +1,15 @@
+"use strict";
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Jane User";
+var use = "x";
+document.body.innerHTML = greeter(user);
+var cosas;
+(function (cosas) {
+    cosas[cosas["cosa1"] = 0] = "cosa1";
+    cosas[cosas["cosa2"] = 1] = "cosa2";
+})(cosas || (cosas = {}));
+// tsc "archivo" traspila el archivo seleccionado a js
+// tsc -init traspila todo el proyecto a js
+// tsc -w agrega watch para traspilar automaticamente, con ctrl+c salimos del watch 
---
+++ laboratorio_3/clase_7-11/app.ts
@@ -0,0 +1,16 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+const use = "x";
+
+document.body.innerHTML = greeter(user);
+enum cosas {
+    cosa1,
+    cosa2
+}
+// tsc "archivo" traspila el archivo seleccionado a js
+// tsc -init traspila todo el proyecto a js
+// tsc -w agrega watch para traspilar automaticamente, con ctrl+c salimos del w
+// federico tomadin - Github
