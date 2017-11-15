a2c16903314604eb50762661c2a10944bc2b678c
alumno <alumno>
2017-11-14 21:05:39 -0300

    add class 14-11

--- laboratorio_3/clase_7-11/app.js
+++ laboratorio_3/clase_7-11/app.js
@@ -12,4 +12,5 @@ var cosas;
 })(cosas || (cosas = {}));
 // tsc "archivo" traspila el archivo seleccionado a js
 // tsc -init traspila todo el proyecto a js
-// tsc -w agrega watch para traspilar automaticamente, con ctrl+c salimos del watch 
+// tsc -w agrega watch para traspilar automaticamente, con ctrl+c salimos del w
+// federico tomadin - Github 
